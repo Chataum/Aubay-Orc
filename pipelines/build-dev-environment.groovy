b6bbf0ddab226e072cdcdd663e4e87b7452e52b1
Chataum <71738541+Chataum@users.noreply.github.com>
2023-02-02 17:14:32 -0300

    Update build-dev-environment.groovy

--- pipelines/build-dev-environment.groovy
+++ pipelines/build-dev-environment.groovy
@@ -53,7 +53,7 @@ pipeline {
                 """
 
                 sh """
-                docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="$params.MYSQL_PASSWORD" < include/create_developer.template'
+                docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="$params.MYSQL_PASSWORD" < scripts/create_developer.template'
                 """
 
                 echo "Docker container created: $containerName"
